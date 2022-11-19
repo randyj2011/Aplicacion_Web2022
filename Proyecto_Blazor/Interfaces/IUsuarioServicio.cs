42292adaa68a74e3b8df065864eda84325a74bc0
Randy Oseguera <116929941+randyj2011@users.noreply.github.com>
2022-11-19 12:04:41 -0600

    commit 4
    Creacion de lista

---
+++ Proyecto_Blazor/Interfaces/IUsuarioServicio.cs
@@ -0,0 +1,13 @@
+﻿using Modelos;
+
+namespace Proyecto_Blazor.Interfaces
+{
+    public interface IUsuarioServicio
+    {
+        Task<Usuario> GetPorCodigo(string codigo);
+        Task<bool> Nuevo(Usuario usuario);
+        Task<bool> Actualizar(Usuario usuario);
+        Task<bool> Eliminar(string codigo);
+        Task<IEnumerable<Usuario>> GetLista();
+    }
+}
---
+++ Proyecto_Blazor/Pages/MisUsuarios/Usuarios.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using Proyecto_Blazor.Interfaces;
+
+namespace Proyecto_Blazor.Pages.MisUsuarios
+{
+    public partial class Usuarios
+    {
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+
+        private IEnumerable<Usuario> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioServicio.GetLista();
+        }
+    }
+}
